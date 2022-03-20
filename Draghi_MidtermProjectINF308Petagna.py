6b36c934e0d3db1d72228d9a2a1d64cf4409ed8f
Kyle Draghi <kdraghi@albany.edu>
2022-03-19 20:29:59 -0400

    Define Main Menu function

--- Draghi_MidtermProjectINF308Petagna.py
+++ Draghi_MidtermProjectINF308Petagna.py
@@ -3,3 +3,13 @@
 
 # Create an empty dictionary
 inventory = {}
+
+# Main Menu
+def mainMenu():
+    print("############# Main Menu #############")
+    print("1: Add items(s) to your inventory")
+    print("2: Remove item(s) in your inventory")
+    print("3: Update amount of item(s) in your inventory")
+    print("4: View your inventory")
+    print("5: Clear your inventory and start over")
+    print("6: Exit")
