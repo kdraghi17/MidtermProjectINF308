971bb379eb1d90a17082a8da308bf987834d9735
Kyle Draghi <kdraghi@albany.edu>
2022-03-19 20:37:01 -0400

    Ask user how many items to add

--- Draghi_MidtermProjectINF308Petagna.py
+++ Draghi_MidtermProjectINF308Petagna.py
@@ -13,3 +13,15 @@ def mainMenu():
     print("4: View your inventory")
     print("5: Clear your inventory and start over")
     print("6: Exit")
+
+# Prompt user to enter how many item(s) they want to add to the inventory
+def add_to_inventory():
+    # Use Try/Except for user value error
+    while True:
+        try:
+            n = int(input("How many items would you like to add to your inventory?: "))
+        except ValueError:
+            print("Invalid input. Not an integer. Please try again.")
+            continue
+        else:
+            break
